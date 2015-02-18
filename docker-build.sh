d3ec405d05f99766c41caf7dc270320b51899617
joe miller <joeym@joeym.net>
2015-02-18 14:55:44 -0800

    run in interactive mode so that progress of long running commands is visible on console

--- docker-build.sh
+++ docker-build.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
+# starts a docker container and kicks off rpm-build.sh inside of it
 
 CURRENT_DIR="$(dirname "$(readlink -f $0)")"
 
 build_image=quay.io/getpantheon/rpmbuild-fedora-20
 
 docker pull $build_image
-docker run --rm \
+docker run --rm -it \
     -w /root/rpmbuild/src \
     -v $CURRENT_DIR:/root/rpmbuild/src \
     $build_image \
